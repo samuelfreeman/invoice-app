a608bf58c89e2691277eb29cd915d075253778eb
samuelfreeman <samuelmanueljnr@gmail.com>
2024-06-17 18:03:40 +0000

    completed the nextlearn course

---
+++ nextjs-dashboard/app/dashboard/customers/page.tsx
@@ -0,0 +1,23 @@
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import CustomersTable from '@/app/ui/customers/table';
+
+
+
+export default async function Page({
+    searchParams,
+}: {
+    searchParams?: {
+        query?: string;
+        page?: string;
+    };
+}) {
+    const query = searchParams?.query || '';
+
+    const customers = await fetchFilteredCustomers(query);
+
+    return (
+        <main>
+            <CustomersTable customers={customers} />
+        </main>
+    );
+}
